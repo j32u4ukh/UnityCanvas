f8b0553fe5921065e38525198dbc103f91f65ad2
ETLab-ite <etlab.ite@gmail.com>
2021-05-18 14:40:09 +0800

    開啟檔案選擇視窗，並返回檔案的絕對路徑

---
+++ Assets/_Scripts/FileDialog.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileDialog : MonoBehaviour
+{
+    public Button button;
+    public RawImage raw_image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(()=>{
+            Apply();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [MenuItem("Example/Overwrite Texture")]
+    void Apply()
+    {
+        //Texture2D texture = Selection.activeObject as Texture2D;
+        //if (texture == null)
+        //{
+        //    EditorUtility.DisplayDialog("Select Texture", "You must select a texture first!", "OK");
+        //    return;
+        //}
+
+        Texture2D texture = raw_image.texture as Texture2D;
+
+        string path = EditorUtility.OpenFilePanel("Overwrite with png", "", "png");
+        print($"path: {path}");
+        //if (path.Length != 0)
+        //{
+        //    var fileContent = File.ReadAllBytes(path);
+        //    texture.LoadImage(fileContent);
+        //}
+
+        raw_image.texture = texture;
+    }
+}
