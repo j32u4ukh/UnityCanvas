0f77f73837f01dd9367560a2571b052444241f40
ETLab-ite <etlab.ite@gmail.com>
2021-05-18 18:21:31 +0800

    初步可拖動 UI 元素，放開滑鼠後返回原始位置

---
+++ Assets/_Scripts/DragHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject start_dragged_obj;
+    Vector3 start_position;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        start_dragged_obj = gameObject;
+        start_position = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        start_dragged_obj = null;
+        transform.position = start_position;
+    }
+}
